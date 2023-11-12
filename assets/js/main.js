0b3a4f41adee5d9d0bb42b51939a329d1acf7cc9
Syed Taha Faisal <faisasye@sheridancollege.ca>
2023-11-12 17:38:54 -0500

    Completed emails form functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,10 +39,42 @@ const shadowHeader = () =>{
 window.addEventListener('scroll', shadowHeader)
 
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+        contactMessage = document.getElementById('contact-message')
 
+const sendEmail = (e) => {
+    e.preventDefault()
+
+    //serviceID - templateID - #form - publicKey
+    emailjs.sendForm('service_7gshp2l','template_24w4sov','#contact-form','CpW7qzNL5RUcIRaKC')
+    .then(() => {
+        //Show sent message
+        contactMessage.textContent = 'Message sent successfully.'
+
+        //Remove message after 5 seconds
+        setTimeout(() => {
+            contactMessage.textContent = ''
+        }, 5000)
+
+        //Clear input fields
+        contactForm.reset()
+    }, () =>{
+        //error message
+        contactMessage.textContent = 'Message not sent (service error).'
+    })
+
+}
+
+contactForm.addEventListener('submit', sendEmail)
 
 /*=============== SHOW SCROLL UP ===============*/ 
-
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
