f1b09d7711b98dd886b8707785bd1179e2a14549
Syed Taha Faisal <faisasye@sheridancollege.ca>
2023-11-21 00:44:47 -0500

    Completed email mailto

--- assets/js/copyEmail.js
+++
@@ -1,8 +0,0 @@
-function copyFunction() {
-    var copyText = document.getElementById("copyInput");
-
-    copyText.select();
-    copyText.setSelectionRange(0, 99999); 
-    navigator.clipboard.writeText(copyText.value);
-    alert("Copied text: " + copyText.value);
-  }
